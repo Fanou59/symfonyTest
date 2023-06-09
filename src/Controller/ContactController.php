eca1b26daa299b633d7219a54502dd14d6d8ad30
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2023-06-09 09:34:53 +0200

    cnx, inscription et deconnexion

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function contact(): Response
     {
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
